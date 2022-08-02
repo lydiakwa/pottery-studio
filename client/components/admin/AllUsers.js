e433d42609ee091373a4af785c95944077f6b3b1
Lydia Kwa <lydiakwa@gmail.com>
2022-08-02 16:21:18 +0200

    REFACTOR: removed conditional on all users that they should be admin this is already defined in App.js with the routes

--- client/components/admin/AllUsers.js
+++ client/components/admin/AllUsers.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUsers } from '../../store/users';
-import { autoLogin } from '../../store/auth';
 
 function AllUsers() {
-  const auth = useSelector((state) => state.auth);
   const users = useSelector((state) => state.users);
 
   const dispatch = useDispatch();
@@ -15,43 +13,34 @@ function AllUsers() {
 
   return (
     <div className="container">
-      {auth.isAdmin ? (
-        <div>
-          <h2>All Users</h2>
+      <div>
+        <h2>All Users</h2>
 
-          <table className="table mt-5">
-            <thead>
-              <tr>
-                {/* <th scope="col">#</th> */}
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Address</th>
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              {/* <th scope="col">#</th> */}
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user.id}>
+                {/* <th scope="row">{user.id}</th> */}
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.address}</td>
               </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <>
-                  <tr key={user.id}>
-                    {/* <th scope="row">{user.id}</th> */}
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.address}</td>
-                  </tr>
-                </>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : null}
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
-const mapDispatch = (dispatch) => ({
-  autoLogin: (token) => dispatch(autoLogin(token)),
-  getUsers: () => dispatch(fetchUsers()),
-});
 
 export default AllUsers;
