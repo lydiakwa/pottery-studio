28a8dc3c5b8f1767bd2dea27e4ecad3d7cc36c6d
Lydia Kwa <lydiakwa@gmail.com>
2022-08-07 14:28:54 +0200

    REFACTOR: unnecessary turnery removed from admin home condition is already set in app.js

--- client/components/admin/AdminHome.js
+++ client/components/admin/AdminHome.js
@@ -7,33 +7,31 @@ function AdminHome() {
 
   return (
     <div className="admin-home-container">
-      {auth.isAdmin ? (
-        <div className="row d-flex justify-content-center">
-          <div className="card mx-5 col-6" style={{ width: '18rem' }}>
-            <img src="user.svg" className="card-img-top" alt="..." />
-            <div className="card-body">
-              <Link
-                to="/admin/users"
-                className="btn btn-dark d-flex justify-content-center"
-              >
-                Manage Users
-              </Link>
-            </div>
-          </div>
-
-          <div className="card mx-5 col-6" style={{ width: '18rem' }}>
-            <img src="management.svg" className="card-img-top mt-4" alt="..." />
-            <div className="card-body">
-              <Link
-                to="/shop"
-                className="btn btn-dark mt-4 d-flex justify-content-center"
-              >
-                Manage Products
-              </Link>
-            </div>
+      <div className="row d-flex justify-content-center">
+        <div className="card mx-5 col-6" style={{ width: '18rem' }}>
+          <img src="user.svg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <Link
+              to="/admin/users"
+              className="btn btn-dark d-flex justify-content-center"
+            >
+              Manage Users
+            </Link>
           </div>
         </div>
-      ) : null}
+
+        <div className="card mx-5 col-6" style={{ width: '18rem' }}>
+          <img src="management.svg" className="card-img-top mt-4" alt="..." />
+          <div className="card-body">
+            <Link
+              to="/shop"
+              className="btn btn-dark mt-4 d-flex justify-content-center"
+            >
+              Manage Products
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
